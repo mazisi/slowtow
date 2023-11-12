61259598a976dd3d6ac6349d94c5b1dfbb3ce9ae
riconyathi <riconyathi@gmail.com>
2023-11-12 15:04:26 +0200

    Licence Type mapping by province

---
+++ database/licence_type_update.sql
@@ -0,0 +1,294 @@
+
+UPDATE `licences` SET `licence_type_id`= 37
+where province = 'Free State'
+and licence_type_id = 1;
+
+UPDATE `licences` SET `licence_type_id`= 45
+where province = 'KwaZulu-Natal'
+and licence_type_id = 1;
+
+UPDATE `licences` SET `licence_type_id`= 59
+where province = 'Limpopo'
+and licence_type_id = 1;
+
+UPDATE `licences` SET `licence_type_id`= 75
+where province = 'Northern Cape'
+and licence_type_id = 1;
+
+UPDATE `licences` SET `licence_type_id`= 88
+where province = 'North West'
+and licence_type_id = 1;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 38
+where province = 'Free State'
+and licence_type_id = 5;
+
+UPDATE `licences` SET `licence_type_id`= 47
+where province = 'KwaZulu-Natal'
+and licence_type_id = 5;
+
+
+UPDATE `licences` SET `licence_type_id`= 80
+where province = 'Northern Cape'
+and licence_type_id = 5;
+
+UPDATE `licences` SET `licence_type_id`= 92
+where province = 'North West'
+and licence_type_id = 5;
+
+
+
+
+UPDATE `licences` SET `licence_type_id`= 53
+where province = 'KwaZulu-Natal'
+and licence_type_id = 6;
+
+UPDATE `licences` SET `licence_type_id`= 63
+where province = 'Limpopo'
+and licence_type_id = 6;
+
+UPDATE `licences` SET `licence_type_id`= 83
+where province = 'Northern Cape'
+and licence_type_id = 6;
+
+UPDATE `licences` SET `licence_type_id`= 95
+where province = 'North West'
+and licence_type_id = 6;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 55
+where province = 'Limpopo'
+and licence_type_id = 7;
+
+UPDATE `licences` SET `licence_type_id`= 71
+where province = 'Northern Cape'
+and licence_type_id = 7;
+
+
+
+
+UPDATE `licences` SET `licence_type_id`= 52
+where province = 'KwaZulu-Natal'
+and licence_type_id = 8;
+
+UPDATE `licences` SET `licence_type_id`= 62
+where province = 'Limpopo'
+and licence_type_id = 8;
+
+UPDATE `licences` SET `licence_type_id`= 82
+where province = 'Northern Cape'
+and licence_type_id = 8;
+
+UPDATE `licences` SET `licence_type_id`= 96
+where province = 'North West'
+and licence_type_id = 8;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 32
+where province = 'Eastern Cape'
+and licence_type_id = 9;
+
+UPDATE `licences` SET `licence_type_id`= 39
+where province = 'Free State'
+and licence_type_id = 9;
+
+UPDATE `licences` SET `licence_type_id`= 54
+where province = 'KwaZulu-Natal'
+and licence_type_id = 9;
+
+UPDATE `licences` SET `licence_type_id`= 61
+where province = 'Limpopo'
+and licence_type_id = 9;
+
+UPDATE `licences` SET `licence_type_id`= 67
+where province = 'Mpumalanga'
+and licence_type_id = 9;
+
+UPDATE `licences` SET `licence_type_id`= 84
+where province = 'Northern Cape'
+and licence_type_id = 9;
+
+UPDATE `licences` SET `licence_type_id`= 97
+where province = 'North West'
+and licence_type_id = 9;
+
+UPDATE `licences` SET `licence_type_id`= 101
+where province = 'Western Cape'
+and licence_type_id = 9;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 40
+where province = 'Free State'
+and licence_type_id = 13;
+
+UPDATE `licences` SET `licence_type_id`= 46
+where province = 'KwaZulu-Natal'
+and licence_type_id = 13;
+
+UPDATE `licences` SET `licence_type_id`= 91
+where province = 'North West'
+and licence_type_id = 13;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 49
+where province = 'KwaZulu-Natal'
+and licence_type_id = 16;
+
+UPDATE `licences` SET `licence_type_id`= 94
+where province = 'North West'
+and licence_type_id = 16;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 41
+where province = 'Free State'
+and licence_type_id = 17;
+
+UPDATE `licences` SET `licence_type_id`= 44
+where province = 'KwaZulu-Natal'
+and licence_type_id = 17;
+
+UPDATE `licences` SET `licence_type_id`= 56
+where province = 'Limpopo'
+and licence_type_id = 17;
+
+UPDATE `licences` SET `licence_type_id`= 72
+where province = 'Northern Cape'
+and licence_type_id = 17;
+
+UPDATE `licences` SET `licence_type_id`= 86
+where province = 'North West'
+and licence_type_id = 17;
+
+
+UPDATE `licences` SET `licence_type_id`= 65
+where province = 'Limpopo'
+and licence_type_id = 18;
+
+UPDATE `licences` SET `licence_type_id`= 76
+where province = 'Northern Cape'
+and licence_type_id = 18;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 60
+where province = 'Limpopo'
+and licence_type_id = 19;
+
+UPDATE `licences` SET `licence_type_id`= 77
+where province = 'Northern Cape'
+and licence_type_id = 19;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 48
+where province = 'KwaZulu-Natal'
+and licence_type_id = 20;
+
+UPDATE `licences` SET `licence_type_id`= 81
+where province = 'Northern Cape'
+and licence_type_id = 20;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 42
+where province = 'Free State'
+and licence_type_id = 21;
+
+UPDATE `licences` SET `licence_type_id`= 50
+where province = 'KwaZulu-Natal'
+and licence_type_id = 21;
+
+UPDATE `licences` SET `licence_type_id`= 78
+where province = 'Northern Cape'
+and licence_type_id = 21;
+
+UPDATE `licences` SET `licence_type_id`= 93
+where province = 'North West'
+and licence_type_id = 21;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 51
+where province = 'KwaZulu-Natal'
+and licence_type_id = 22;
+
+UPDATE `licences` SET `licence_type_id`= 58
+where province = 'Limpopo'
+and licence_type_id = 22;
+
+UPDATE `licences` SET `licence_type_id`= 73
+where province = 'Northern Cape'
+and licence_type_id = 22;
+
+UPDATE `licences` SET `licence_type_id`= 87
+where province = 'North West'
+and licence_type_id = 22;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 36
+where province = 'Free State'
+and licence_type_id = 25;
+
+UPDATE `licences` SET `licence_type_id`= 43
+where province = 'KwaZulu-Natal'
+and licence_type_id = 25;
+
+UPDATE `licences` SET `licence_type_id`= 85
+where province = 'North West'
+and licence_type_id = 25;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 79
+where province = 'Nothern Cape'
+and licence_type_id = 27;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 33
+where province = 'Eastern Cape'
+and licence_type_id = 28;
+
+UPDATE `licences` SET `licence_type_id`= 68
+where province = 'Mpumalanga'
+and licence_type_id = 28;
+
+UPDATE `licences` SET `licence_type_id`= 98
+where province = 'Western Cape'
+and licence_type_id = 28;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 34
+where province = 'Eastern Cape'
+and licence_type_id = 30;
+
+UPDATE `licences` SET `licence_type_id`= 69
+where province = 'Mpumalanga'
+and licence_type_id = 30;
+
+UPDATE `licences` SET `licence_type_id`= 99
+where province = 'Western Cape'
+and licence_type_id = 30;
+
+
+
+UPDATE `licences` SET `licence_type_id`= 35
+where province = 'Eastern Cape'
+and licence_type_id = 31;
+
+UPDATE `licences` SET `licence_type_id`= 70
+where province = 'Mpumalanga'
+and licence_type_id = 31;
+
+UPDATE `licences` SET `licence_type_id`= 100
+where province = 'Western Cape'
+and licence_type_id = 31;
