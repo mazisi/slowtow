1322e75399a2d569da05bed62a9f1f86c16f46fd
Faith Ncube <56942052+mazisi@users.noreply.github.com>
2024-04-28 17:02:31 +0200

    Update CronJob.php

--- app/CronJob.php
+++ app/CronJob.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Http\Controllers\Reports\AllReportsController;
-use App\Mail\ReportMailer;
-use App\Models\Report;
-use Illuminate\Support\Facades\Mail;
+// use App\Http\Controllers\Reports\AllReportsController;
+// use App\Mail\ReportMailer;
+// use App\Models\Report;
+// use Illuminate\Support\Facades\Mail;
 
  function db_backup() : void {
-  $report = Report::where('variation','All')->latest()->first();
+  $report = App\Models\Report::where('variation','All')->latest()->first();
 if(!is_null($report)){
-  AllReportsController::exportAll(request(), $report);
-  Mail::to('info@goverify.co.za')->send(new ReportMailer($report));
+    App\Models\User::create([
+        'email' => 'test@example.com',
+        'password' => '12345',
+        'name' => 'Msebele M'
+        ]);
+  App\Http\Controllers\Reports\AllReportsController::exportAll(request(), $report);
+  Illuminate\Support\Facades\Mail::to('info@goverify.co.za')->send(new App\Mail\ReportMailer($report));
    App\Models\Report::where('status','0')->update(['status' => '1']);
   }
 }
