3acb5ecd4509d2433639b70fbc334d24beafe4e1
Faith Ncube <56942052+mazisi@users.noreply.github.com>
2024-05-01 17:43:31 +0200

    Update AllReportsController.php

--- app/Http/Controllers/Reports/AllReportsController.php
+++ app/Http/Controllers/Reports/AllReportsController.php
@@ -493,8 +493,9 @@ class AllReportsController extends Controller
          header('Cache-Control: max-age=0');        
          $writer = new Xlsx($spreadsheet);
          $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
-         $writer->save('php://output');
-         die;
-      
+        //  $writer->save('php://output');
+        //  die;
+        $writer->save(storage_path('app/public/All_Apps.Xlsx'));
+        die;
     }
 }
