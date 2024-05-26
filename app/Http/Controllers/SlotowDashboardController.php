a257c4c8b37d2da1c6a18221753f195bf03759c1
Faith Ncube <56942052+mazisi@users.noreply.github.com>
2024-05-26 19:37:37 +0200

    Revert V1 Dashboard

--- app/Http/Controllers/SlotowDashboardController.php
+++ app/Http/Controllers/SlotowDashboardController.php
@@ -3,126 +3,61 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Models\People;
+use App\Models\Company;
 use App\Models\Licence;
-use App\Models\LicenceRenewal;
+use Illuminate\Http\Request;
 use App\Models\TemporalLicence;
 use Illuminate\Support\Facades\DB;
 
 class SlotowDashboardController extends Controller
 {
-
-
     public function index(){
-        $years = DB::table('years')->orderBy('year', 'DESC')->get()->pluck('year');
-   
+        $licences = Licence::count();
+        $companies = Company::count();
+        $people = People::count();
+        $temp_licences = TemporalLicence::count();
+
+        $jan = Licence::whereMonth('created_at','01')->count();
+        $feb = Licence::whereMonth('created_at','02')->count();
+        $mar = Licence::whereMonth('created_at','03')->count();
+        $apr = Licence::whereMonth('created_at','04')->count();
+        $may = Licence::whereMonth('created_at','05')->count();
+        $jun = Licence::whereMonth('created_at','06')->count();
+        $jul = Licence::whereMonth('created_at','07')->count();
+        $aug = Licence::whereMonth('created_at','08')->count();
+        $sep = Licence::whereMonth('created_at','09')->count();
+        $oct = Licence::whereMonth('created_at','10')->count();
+        $nov = Licence::whereMonth('created_at','11')->count();
+        $dec = Licence::whereMonth('created_at','12')->count();
+
+        // $count_group_licences = DB::table('licences')
+        //                         ->select(DB::raw('MONTH(licence_date) as month, count(*) as number'))
+        //                         ->whereNotNull('licence_date')
+        //                         ->groupByRaw('MONTH(licence_date)')
+        //                         ->orderByRaw('MONTH(licence_date) asc')
+        //                         ->get();
+
+                              
+
+
         return Inertia::render('Dashboard',[
-        'licences' => $this->newLicences(),
-        'renewals' => $this->renewals(),
-        'years' => $years,
-        'tempLicences' => $this->tempLicences(),
+        'licences' => $licences,
+        'companies' => $companies,
+        'people' => $people,
+        'temp_licences' => $temp_licences,
+        'jan' => $jan,
+        'feb' => $feb,
+        'mar' => $mar,
+        'apr' => $apr,
+        'may'=> $may,
+        'jun'=> $jun,
+        'jul'=> $jul,
+        'aug'=> $aug,
+        'sep'=> $sep,
+        'oct'=> $oct,
+        'nov'=> $nov,
+        'dec' => $dec,
         ]);
-    }
-
-    function newLicences() {
-        $currentYear = now()->year;
-        $currentMonth = now()->month;    
-        $defaultProvince = 'Gauteng';
-
-        $licencesByMonth = Licence::select(
-            DB::raw('MONTH(created_at) as month'),
-            DB::raw('COUNT(*) as count')
-        )
-        ->when(request('year', $currentYear), function ($query, $year) {
-            $query->whereYear('created_at', $year);
-        })
-        ->when(request('month', $currentMonth), function ($query, $month) {
-            $query->whereMonth('created_at', $month);
-        })
-        ->when(request('province', $defaultProvince), function ($query, $province) {
-            $query->where('province', $province);
-        })
-        ->groupBy(DB::raw('MONTH(created_at)'))
-        ->pluck('count', 'month');
-    
-        // Initialize an array with all months set to 0
-        $monthlyLicenceCounts = array_fill(1, 12, 0);
-    
-        // Fill the monthly counts with actual data
-        foreach ($licencesByMonth as $month => $count) {
-            $monthlyLicenceCounts[$month] = $count;
-        }
-    
-        return $monthlyLicenceCounts;
-    }
-    
-    
-
-    function renewals() {  
-
-        $currentYear = now()->year;
-        $currentMonth = now()->month;    
-        $defaultProvince = 'Gauteng';
-        
-        $renewalsByMonth = LicenceRenewal::select(
-            DB::raw('MONTH(created_at) as month'),
-            DB::raw('COUNT(*) as count')
-        )
-        ->when(request('year', $currentYear), function ($query, $year) {
-            $query->whereYear('created_at', $year);
-        })
-        ->when(request('month', $currentMonth), function ($query, $month) {
-            $query->whereMonth('created_at', $month);
-        })
-
-        ->when(request('province', $defaultProvince), function ($query, $province) {
-            $query->whereHas('licence', function ($query) use ($province) {
-                $query->where('province', $province);
-            });
-        })
-        ->groupBy(DB::raw('MONTH(created_at)'))
-        ->pluck('count', 'month');
-    
-        // Initialize an array with all months set to 0
-        $monthlyRenewalCounts = array_fill(1, 12, 0);
-    
-        // Fill the monthly counts with actual data
-        foreach ($renewalsByMonth as $month => $count) {
-            $monthlyRenewalCounts[$month] = $count;
-        }
-    
-        return $monthlyRenewalCounts;
-    }
-
-    function tempLicences() { 
-        
-        $currentYear = now()->year;
-        $currentMonth = now()->month;    
-        $defaultProvince = 'Gauteng';
-        
-        $tempLicences = TemporalLicence::select(
-            DB::raw('MONTH(created_at) as month'),
-            DB::raw('COUNT(*) as count')
-        )
-        ->when(request('year', $currentYear), function ($query, $year) {
-            $query->whereYear('created_at', $year);
-        })
-        ->when(request('month', $currentMonth), function ($query, $month) {
-            $query->whereMonth('created_at', $month);
-        })
-        ->groupBy(DB::raw('MONTH(created_at)'))
-        ->pluck('count', 'month');
-    
-        // Initialize an array with all months set to 0
-        $monthlyTempLicenceCounts = array_fill(1, 12, 0);
-    
-        // Fill the monthly counts with actual data
-        foreach ($tempLicences as $month => $count) {
-            $monthlyTempLicenceCounts[$month] = $count;
-        }
-    
-        return $monthlyTempLicenceCounts;
-
-
-        
     }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,58 +1,148 @@
 <template>
-    <Layout>
-      <Head title="Dashboard" />
-      <div class="py-5 container-fluid">
-       <div class="row">
-        <div class="col-3">
-          <div class="input-group input-group-outline null is-filled">
-          <Multiselect                
-          :options="computedProvinces"
-           v-model="form.province"
-           @select="filter"
-          :taggable="true"
-          placeholder="Filter By Province"/>
+  <Layout>
+    <Head title="Dashboard" />
+    <div class="py-4 container-fluid mt-2">
+      <div class="row mb-4">
+        <div class="col-lg-12 position-relative z-index-2">
+          <div class="row">
+            <Link :href="`/licences`" class="col-lg-3 col-md-6 col-sm-6">
+              <mini-statistics-card
+                :title="{ text: 'Licences', value: licences }"
+                :icon="{
+                  name: 'wine_bar',
+                  color: 'text-white',
+                  background: 'dark',
+                }"
+              />
+            </Link>
+            <Link :href="`/companies`" class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4">
+              <mini-statistics-card
+                :title="{ text: 'Companies', value: companies }"
+                :icon="{
+                  name: 'store',
+                  color: 'text-white',
+                  background: 'primary',
+                }"
+              />
+            </Link>
+            <Link :href="`/people`" class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4">
+              <mini-statistics-card
+                :title="{ text: 'People', value: people }"
+                :icon="{
+                  name: 'groups',
+                  color: 'text-white',
+                  background: 'success',
+                }"
+              />
+              </Link>
+            <Link :href="`/temp-licences`" class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4">
+              <mini-statistics-card
+                :title="{ text: 'Temporary Licences', value: temp_licences }"
+                :icon="{
+                  name: 'schedule',
+                  color: 'text-white',
+                  background: 'info',
+                }"
+              />
+              </Link>
           </div>
-
-        </div>
-
-        <div class="col-3">
-          <div class="input-group input-group-outline null is-filled">
-          <Multiselect                
-          :options="years"
-           v-model="form.year"
-           @select="filter"
-          :taggable="true"
-          placeholder="Filter By Year"/>
-          </div>
-
-        </div>
-
-        <div class="col-3">
-          <div class="input-group input-group-outline null is-filled">
-          <select @change="filter" class="form-control form-control-default is-filled" v-model="form.month">
-            <option :value="''" disabled :selected="true">Filter By Month</option>
-            <option v-for="month in months" :key="month.id" :value="month.id">{{ month.name }}</option>
-          </select>
-          </div>
-
-        </div>
-        <div class="col-3">
-          <button class="btn btn-sm btn-primary" type="button" @click="resetFilter">Reset</button>
-
-        </div>
-
-       </div>
-       
-        <div class="row">
-          <div class="col-xl-12 col-lg-12">
-              <div class="card l-bg-cherry">
-                <Chart type="line" :data="chartData" :options="chartOptions" class="h-30rem" style="height: 574px;"/>
-              </div>
+        
+          <div class=" row mt-4">
+            <div class="d-none col-lg-6 col-md-6 mt-4">
+              <chart-holder-card
+                title="Licences"
+              >
+                <reports-bar-chart
+                  :chart="{
+                    labels: month,
+                    datasets: {
+                      label: 'Licences',
+                      data: [50, 80, 20, 10, 22, 50, 10, 40, 55, 70, 65, 15, 25],
+                    },
+                  }"
+                />
+              </chart-holder-card>
             </div>
-          
+            <div class="invisible col-12 col-12 mt-4">
+              <chart-holder-card
+                title="Licences"
+                color="success"
+              >
+                <reports-line-chart
+                  :chart="{
+                    labels: [
+                      'Jan',
+                      'Feb',
+                      'Mar',
+                      'Apr',
+                      'May',
+                      'Jun',
+                      'Jul',
+                      'Aug',
+                      'Sep',
+                      'Oct',
+                      'Nov',
+                      'Dec',
+                    ],
+                    datasets: {
+                      label: 'Licences',
+                      data: [jan, feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec],
+                    },
+                  }"
+                />
+              </chart-holder-card>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
-      </Layout>
-    </template>
-    <script src="./Charts/barChart.js"></script>
-    <style src="@vueform/multiselect/themes/default.css"></style>
+  
+     
+    </div>
+    </Layout>
+  </template>
+  <script>
+  import Layout from "../Shared/Layout.vue";
+  import ChartHolderCard from "./components/ChartHolderCard.vue";
+  import ReportsBarChart from "@/examples/Charts/ReportsBarChart.vue";
+  import ReportsLineChart from "@/examples/Charts/ReportsLineChart.vue";
+  import MiniStatisticsCard from "./components/MiniStatisticsCard.vue";
+  import { Link, Head } from '@inertiajs/inertia-vue3';
+  
+  export default {
+    name: "dashboard",
+    props: {
+      licences: Number,
+      companies: Number,
+      people: Number,
+      temp_licences: Number,
+      count_group_licences: Object,
+      jan: Number,
+      feb: Number,
+      mar: Number,
+      apr: Number,
+      may: Number,
+      jun: Number,
+      jul: Number,
+      aug: Number,
+      sep: Number,
+      oct: Number,
+      nov:Number,
+      dec: Number
+    },
+    data() {
+      return {
+       
+      };
+    },
+  
+    components: {
+      ChartHolderCard,
+      ReportsBarChart,
+      ReportsLineChart,
+      MiniStatisticsCard,
+      Layout,
+      Link,
+      Head
+  },
+  };
+  </script>
