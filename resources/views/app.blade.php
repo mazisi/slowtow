1fa68803b412917821c05ed0794436999f6048a4
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-08-12 22:39:08 +0200

    Fix font-awesome issues

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -221,7 +221,6 @@ main{
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" ></script>
 
-       <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
 
     </body>
 
