4d9aa8360400402433bbe9bab57e0609d08057ae
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-09-29 22:08:47 +0200

    Update ReportController.php

--- app/Http/Controllers/Reports/ReportController.php
+++ app/Http/Controllers/Reports/ReportController.php
@@ -110,11 +110,11 @@ class ReportController extends Controller
               'email' => auth()->user()->email
             ]);
 
-            // if($request->report_type == 'wholesale'){
-            //   AllWholesaleReportsController::exportAll($request);
-            // }else{
-            //   AllReportsController::exportAll($request);
-            // }
+            if($request->report_type == 'wholesale'){
+              AllWholesaleReportsController::exportAll($request);
+            }else{
+              AllReportsController::exportAll($request);
+            }
             return back()->with('success','Report is being generated. Please check your email');
             break;
             case 'Renewals':
